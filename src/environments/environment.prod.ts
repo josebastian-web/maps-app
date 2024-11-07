8e9bce72f6c75999ebcaa31b2392fa73ea1fbee7
Jose Lagos <jose.lagos2013@yahoo.com>
2024-11-07 19:08:57 -0300

    config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiKey: 'pk.eyJ1Ijoiam9zZS1sYWdvcyIsImEiOiJjbTJ1bnhpZzEwM2JqMmtxNnRxdWxjbnl3In0.hlE8_txW9hUHtvlt_JovqQ'
+  // PUT API KEY MAP-BOX
+  apiKey: ''
 }
